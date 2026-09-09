d79c9cf3450b2aa966668698125b0b9434e4a75b
so4tcoding <so4tcoding@gmail.com>
2026-09-09 21:13:33 +0900

    Add files via upload

---
+++ start_kiosk.ps1
@@ -0,0 +1,6 @@
+﻿Write-Host "키오스크 로컬 서버 세팅 중..." -ForegroundColor Cyan
+Start-Job {
+    Start-Sleep -Seconds 2
+    Start-Process "http://localhost:8000/kiosk.html"
+} | Out-Null
+python -m http.server 8000
